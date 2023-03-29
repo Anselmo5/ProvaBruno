2b7f2f93f8abe703f6451e0595d67ab19b7d4139
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-29 12:35:40 -0300

    Create ProvaBruno.js

---
+++ ProvaBruno.js
@@ -0,0 +1,15 @@
+class pai{
+    constructor(nome,idade,aniversario){
+    this.nome = nome
+    this.idade = idade
+    this.aniversario = aniversario
+    } 
+ 
+    painome(){
+        console.log("esse e meu nome "+this.nome);
+    }
+
+    idadepai(){
+        console.log("essa e minha idade "+this.idade);
+    }
+}
